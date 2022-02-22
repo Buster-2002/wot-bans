4cb399944bfc0737c6dcafa9fad727ea4f7ddfcf
Buster <mysteriousduck2002@gmail.com>
2022-02-22 16:12:37 +0100

    some fixes and save to file

--- wot-bans/gbadges.py
+++ wot-bans/gbadges.py
@@ -5,24 +5,32 @@
 import json
 from pathlib import Path
 
-REGION = input('Region > ')
+from tabulate import tabulate
+
+REGION = input('Region > ').lower()
 FILENAME = input('Filename > ')
 CLAN_AMOUNT = int(input('Clan amount > '))
 PLAYER_AMOUNT = int(input('Player amount > '))
 
-def main(file_path: Path):
+def main(file_path: Path) -> dict:
     gbadge_gamers = {}
     with open(file_path, encoding='utf-8') as file:
         data = json.load(file)
 
     for player_id, player_data in data.items():
-        if (player_data['player_rank'] <= (PLAYER_AMOUNT / 100)) \
-        and (player_data['clan_rank'] <= (CLAN_AMOUNT / 100)):
-            gbadge_gamers[player_id] = player_data
-            print(player_data['player_name'])
+        if all([player_data['player_rank'], player_data['clan_rank']]):
+            if (player_data['player_rank'] <= (PLAYER_AMOUNT / 100)) \
+            and (player_data['clan_rank'] <= (CLAN_AMOUNT / 100)):
+                gbadge_gamers[player_id] = player_data
 
     return gbadge_gamers
 
-
 if __name__ == '__main__':
-    main(Path(f'globalmap_data/{REGION}/{FILENAME}.json'))
+    gbadges = main(Path(f'globalmap_data/{REGION}/{FILENAME}.json'))
+    formatted = []
+
+    for i, (player_id, player_data) in enumerate(gbadges.items(), 1):
+        formatted.append([str(i).zfill(3), player_data['player_name'], player_data['player_rank'], player_data['clan_tag'], player_data['clan_rank']])
+
+    with open(f'globalmap_data/{REGION}/_gbadges.txt', 'w', encoding='utf-8') as file:
+        file.write(tabulate(formatted, headers=['Index', 'Name', 'Rank', 'Clan', 'Rank']))
