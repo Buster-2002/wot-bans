a260032d936b7d820cd4f2c0d1fed31b39e93abd
Buster <mysteriousduck2002@gmail.com>
2022-03-19 14:48:19 +0100

    moved some text to utils for translate purposes, improved stats_link

--- wot-bans/globalmap.py
+++ wot-bans/globalmap.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-'''gm_bans.py: Gets global map player leaderboards and formats final data to applicable markdown'''
+'''gm_bans.py: Gets global map player leaderboards and formats final data to applicable markdown
+
+The MIT License (MIT)
+
+Copyright (c) 2021-present Buster
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+'''
 
 import time
 from collections import Counter
@@ -20,7 +43,6 @@ __license__ = 'MIT'
 init(autoreset=True)
 SESSION = requests.Session()
 SESSION.headers = {'x-requested-with': 'XMLHttpRequest'}
-YES = {'yes', 'y', 'true', 't', '1', 'enable', 'on'}
 PLAYER_BAN_HEADER = '''
 ## Banned Players
 
@@ -39,78 +61,12 @@ NEW_RECEIVERS_HEADER = '''
 | Index | Player | Player Rank |
 |:--------------:|:--------------:|:--------------:|
 '''.strip()
-ENGLISH_TEXT = '''
-# Player bans for the {logo} {title} campaign ({region})
-*Made by {author}*
-
-## General
-
-The raw data and the code used is available in the GitHub repo [here](https://github.com/Buster-2002/wot-bans/).
-
-Below follows a list of the names and related statistics of **{amount_banned}** players in the {region} region that were disqualified from the leaderboard. They will not receive any rewards, and depending on previous offenses might be permanently banned from playing. 
-
-> Using prohibited modifications or violating the game or [event_id rules]({regulations}) in any way leads to exclusion from the Alley of Fame, thereby preventing violators from receiving rewards.
-> 
-> \- *World of Tanks Fair Play Policy*
-
-Also check out:  
-- [Global Map Legend badge receivers]({gbadges_url})
-- [Clan ranking by reward tanks]({tankranking_url})
-
-Note that I am only able to know the banned players who were on the leaderboard at the time of the event_id ending.
-'''.strip()
-RUSSIAN_TEXT = '''
-# Блокировка игроков в кампании {logo} {title} ({region})
-*Сделано {author}*
-
-## Общий
-
-Необработанные данные для этой кампании и используемый код доступны в репозитории GitHub [здесь](https://github.com/Buster-2002/wot-bans/).
-
-Ниже приводится список имен и связанная с ними статистика **{amount_banned}** игроков в регионе {region}, которые были дисквалифицированы из таблицы лидеров. Они не получат никаких наград, и в зависимости от предыдущих нарушений могут быть навсегда заблокированы от игры.
-
-> Использование запрещенных модификаций или нарушение игры или [правил события]({regulations}) каким-либо образом влечет за собой исключение из Аллеи славы, тем самым лишая нарушителей возможности получения наград.
->
-> \- *Политика честной игры в World of Tanks*
-
-Также проверьте:
-- [получатели значков Global Map Legend]({gbadges_url})
-- [Рейтинг клана по призовым танкам]({tankranking_url})
-
-Обратите внимание, что я могу узнать только тех забаненных игроков, которые были в таблице лидеров на момент окончания события. 
-'''.strip()
-MANDARIN_TEXT = '''
-# {logo} {title} 活动 ({region}) 的玩家禁令
-*由{author}制作*
-
-＃＃ 一般的
-
-此活动的原始数据和使用的代码可在 GitHub 存储库 [此处](https://github.com/Buster-2002/wot-bans/) 上找到。
-
-以下是 {region} 地区被取消排行榜资格的 **{amount_banned}** 玩家的姓名和相关统计数据列表。 他们将不会获得任何奖励，并且根据之前的违规行为可能会被永久禁止参加比赛。
-
-> 以任何方式使用禁止的修改或违反游戏或[活动规则]({regulations})导致被排除在名人堂之外，从而阻止违反者获得奖励。
->
-> \- *坦克世界公平竞赛政策*
-
-另请查看：
-- [全球地图图例徽章接收器]({gbadges_url})
-- [战队奖励坦克排名]({tankranking_url})
-
-请注意，我只能知道活动结束时在排行榜上的被禁玩家。 
-'''.strip()
-REGION_TRANSLATIONS = {
-    Region.asia: MANDARIN_TEXT + '\n\n' + ENGLISH_TEXT,
-    Region.russia: RUSSIAN_TEXT + '\n\n' + ENGLISH_TEXT,
-    Region.europe: ENGLISH_TEXT,
-    Region.north_america: ENGLISH_TEXT
-}
 
 
 class GmBans(BanEvaluator):
     def __init__(self, region: Region, front_id: str, event_id: str):
         self.region = region
-        self.link_region = 'com' if region is Region.north_america else str(region)
+        self.link_region = 'com' if region is Region.NORTH_AMERICA else str(region)
         self.leaderboard_url = f"https://worldoftanks.{self.link_region}/en/clanwars/rating/alley/users/?event_id={{0}}&front_id={{1}}&page_size=100&page={{2}}"
 
         self.front_id = front_id
@@ -127,7 +83,7 @@ class GmBans(BanEvaluator):
         Returns:
             str: The formatted data
         '''
-        formatted, start_time = [], time.perf_counter()
+        formatted, start_time, nl = [], time.perf_counter(), '\n'
         data = file_operation(file=filename, op=FileOp.READ)
 
         # Sort banned players by their rank respectively
@@ -139,7 +95,7 @@ class GmBans(BanEvaluator):
 
         # Get clans that have 10 + of their members banned
         clans_with_10plus_bans = list(sorted([
-                (f'[{escape_md(clan_tag)}]({stats_link(clan_tag, self.region, is_clan=True)})', ban_amount)
+                (stats_link(clan_tag, self.region, is_clan=True), ban_amount)
                 for clan_tag, ban_amount in Counter([v['clan_tag']
                 for v in data.values() if v['clan_tag'] is not None
                 ]).items() if ban_amount >= 10
@@ -150,40 +106,40 @@ class GmBans(BanEvaluator):
 
         # Format the markdown with translation
         base_data_url = f'https://github.com/Buster-2002/wot-bans/blob/master/wot-bans/globalmap_data/{self.region}/{self.event_id}/'
-        formatted.append(REGION_TRANSLATIONS[self.region].format(
+        formatted.append(get_description(self.region, BanType.GLOBALMAP).format(
             title=self.event_id.title(),
             region=str(self.region).upper(),
             author=f'[{__author__}](https://discord.com/users/764584777642672160)',
             amount_banned=len(data.keys()),
             logo=f'<img src="https://eu.wargaming.net/globalmap/images/app/features/events/images/{self.event_id}/promo_logo.png" alt="logo" width="30"/>',
-            regulations=f'https://worldoftanks.{self.link_region}/en/content/confrontation-regulations/',
+            regulations=f'https://worldoftanks.{self.link_region}/en/content/{self.event_id}-regulations/',
             gbadges_url=base_data_url + 'gbadges.txt',
             tankranking_url=base_data_url + 'tankranking.txt'
         ))
 
-        formatted.append('\n')
+        formatted.append(nl)
 
         # Add the most clan ban rows
         formatted.append(CLAN_BAN_HEADER)
         for i, (clan_name, ban_amount) in enumerate(clans_with_10plus_bans, 1):
             formatted.append(f'| {i} | {clan_name} | {ban_amount} |')
 
-        formatted.append('\n')
+        formatted.append(nl)
 
         # Add the player ban rows
         formatted.append(PLAYER_BAN_HEADER)
         for i, v in enumerate(data.values(), 1):
-            formatted.append(f'''| {i} | [{escape_md(v['player_name'])}]({stats_link(v['player_name'], self.region)}) | {intcomma(v['player_rank'])} | {f"[{escape_md(v['clan_tag'])}]({stats_link(v['clan_tag'], self.region, is_clan=True)})" if v['clan_tag'] else 'No Clan'} | {intcomma(v['clan_rank']) or 'N/A'} | {intcomma(v['player_fame_points'])} | {v['player_battles_count']} |''')
+            formatted.append(f'''| {i} | {stats_link(v['player_name'], self.region)} | {intcomma(v['player_rank'])} | {stats_link(v['clan_tag'], self.region, is_clan=True)} | {intcomma(v['clan_rank']) or 'N/A'} | {intcomma(v['player_fame_points'])} | {v['player_battles_count']} |''')
 
-        formatted.append('\n')
+        formatted.append(nl)
 
         # Add new receivers rows
         formatted.append(NEW_RECEIVERS_HEADER)
         for i, v in enumerate(self.new_receivers.values(), 1):
-            formatted.append(f'''| {i} | [{escape_md(v['player_name'])}]({stats_link(v['player_name'], self.region)}) | from {v['old_rank']} to {v['new_rank']} | ''')
+            formatted.append(f'''| {i} | {stats_link(v['player_name'], self.region)} | from {v['old_rank']} to {v['new_rank']} | ''')
 
         print_message('formatting to MarkDown', start_time)
-        return '\n'.join(formatted)
+        return nl.join(formatted)
 
 
     def get_leaderboard(self) -> Dict[str, Dict[str, Union[str, int]]]:
